802dae340117ba1ff85dfcdfd4657e32144dd56c
Ankit-on <ankit.23bcon0441@jecrcu.edu.in>
2026-05-04 10:07:53 +0530

    Normal commit

---
+++ Day-3/divide_by_two.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number till where to run:  ";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        if(i%2==0){
+            cout<<i<<" ";
+        }
+    
+    }
+    return 0;
+}
