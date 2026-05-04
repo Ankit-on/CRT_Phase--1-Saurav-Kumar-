781dcb3c93abb58aa8210c854f96ef5b20ff1f6e
Ankit-on <ankit.23bcon0441@jecrcu.edu.in>
2026-05-04 11:37:55 +0530

    Normal commit

---
+++ hollow_sq.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter size of square: ";
+    cin>>a;
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=a;j++){
+            if(i==1||i==a||j==1||j==a){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
